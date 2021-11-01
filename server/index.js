c261c08cf6e7aefec6afe9c3a4fa1e1c20943160
Havoczephyr <havoczephyr@hotmail.com>
2021-11-01 16:34:24 -0400

    Cors fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express")
+const cors = require('cors')
 const app = express()
 app.use(express.json())
+app.use(cors())
 const serverPort = 4040
 
 app.get("/api/users", ((req, res) => {
