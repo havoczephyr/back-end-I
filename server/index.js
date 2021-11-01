1407d833135ed0e910d732791f3ec62528556149
Havoczephyr <havoczephyr@hotmail.com>
2021-11-01 16:16:01 -0400

    completion commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const app = express()
+app.use(express.json())
+const serverPort = 4040
+
+app.get("/api/users", ((req, res) => {
+    let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+    res.status(200).send(friends);
+}))
+
+app.get("/weather/:temperature", ((req, res) => {
+    const {temperature} = req.params;
+    const phrase = `<h3>It was ${temperature} today</h3>`
+    res.status(200).send(phrase)
+}))
+
+
+
+
+app.listen(serverPort, () => console.log(`Server be jammin on port ${serverPort}`))
